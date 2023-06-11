be89f673d8a094df15adaaf8c3e007c0ea3f26b9
gidra5 <hranitel.52@gmail.com>
2023-06-11 13:33:25 +0300

    refactor(zustand-storage): implementation with class

--- zustand-storage/src/index.ts
+++ zustand-storage/src/index.ts
@@ -2,57 +2,56 @@ import { createStore } from 'zustand/vanilla';
 import { devtools } from 'zustand/middleware';
 import { CacheManager, Endpoint, QueryStorage, handlers } from '@cachejs/core';
 
-export const createZustandStorage = (): QueryStorage => {
-  type QueryStore = Record<string, Map<string, unknown>>;
-  const store = createStore(
-    devtools<QueryStore>(() => ({}), { name: 'queryCache', serialize: true })
+// https://docs.pmnd.rs/zustand/guides/maps-and-sets-usage
+export class ZustandQueryStorage<T = unknown> implements QueryStorage<T> {
+  private store = createStore(
+    devtools<Record<string, Map<string, unknown>>>(() => ({}), {
+      name: 'queryCache',
+      serialize: true,
+    })
   );
-  const { getState, setState } = store;
-  // https://docs.pmnd.rs/zustand/guides/maps-and-sets-usage
-  return {
-    get(key, params) {
-      const paramsKey = JSON.stringify(params);
-      return getState()[key]?.get(paramsKey);
-    },
-    set(key, params, value) {
-      const paramsKey = JSON.stringify(params);
-      setState(
-        (queries) => ({
-          ...queries,
-          [key]: new Map(queries[key]).set(paramsKey, value),
-        }),
-        false,
-        { type: key, params }
-      );
-    },
-    has(key, params) {
-      const paramsKey = JSON.stringify(params);
-      return getState()[key]?.has(paramsKey) ?? false;
-    },
-    clear(key, params) {
-      const paramsKey = JSON.stringify(params);
-      setState(
-        (queries) => {
-          const query = queries[key];
-          if (!query) return queries;
-          query.delete(paramsKey);
-          return { ...queries, [key]: new Map(query) };
-        },
-        false,
-        { type: key + '-clear', params }
-      );
-    },
-  };
-};
 
-export const createZustandCacheManager = <
+  get(key: string, params: unknown) {
+    const paramsKey = JSON.stringify(params);
+    return this.store.getState()[key]?.get(paramsKey) as T;
+  }
+  set(key: string, params: unknown, value: T) {
+    const paramsKey = JSON.stringify(params);
+    this.store.setState(
+      (queries) => ({
+        ...queries,
+        [key]: new Map(queries[key]).set(paramsKey, value),
+      }),
+      false,
+      { type: key, params }
+    );
+  }
+  has(key: string, params: unknown) {
+    const paramsKey = JSON.stringify(params);
+    return this.store.getState()[key]?.has(paramsKey) ?? false;
+  }
+  clear(key: string, params: unknown) {
+    const paramsKey = JSON.stringify(params);
+    this.store.setState(
+      (queries) => {
+        const query = queries[key];
+        if (!query) return queries;
+        query.delete(paramsKey);
+        return { ...queries, [key]: new Map(query) };
+      },
+      false,
+      { type: key + '-clear', params }
+    );
+  }
+}
+
+export class ZustandCacheManager<
   Endpoints extends Record<
     string,
-    Endpoint<keyof typeof handlers, (...p: any[]) => Promise<unknown | void>>
+    Endpoint<keyof typeof handlers & string, (...p: any[]) => Promise<unknown>>
   >
->(
-  endpoints: Endpoints
-): CacheManager<typeof handlers, Endpoints> => {
-  const manager = new CacheManager(createZustandStorage(), handlers, endpoints);
-  return manager;
-};
+> extends CacheManager<typeof handlers, Endpoints> {
+  constructor(endpoints: Endpoints) {
+    super(new ZustandQueryStorage(), handlers, endpoints);
+  }
+}
