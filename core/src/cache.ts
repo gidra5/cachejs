4f9b677e695c99359237e2ae1d580afa63e6c83f
gidra5 <hranitel.52@gmail.com>
2023-06-11 22:09:31 +0300

    fix(core): generic query storage tests

--- core/src/cache.ts
+++ core/src/cache.ts
@@ -1,24 +1,26 @@
 import { QueryStorage } from './index.js';
 
 export class GenericQueryStorage<T = unknown> implements QueryStorage<T> {
-  private store: Record<string, Map<string, T>> = {};
+  private store: Map<string, T> = new Map();
+
+  private getStoreKey(key: string, params: unknown) {
+    return JSON.stringify([key, params]);
+  }
 
   get(key: string, params: unknown) {
-    const paramsKey = JSON.stringify(params);
-    return this.store[key]?.get(paramsKey) as T;
+    return this.store.get(this.getStoreKey(key, params)) as T;
   }
   set(key: string, params: unknown, value: T) {
-    const paramsKey = JSON.stringify(params);
-    this.store[key] = new Map(this.store[key]).set(paramsKey, value);
+    const storeKey = this.getStoreKey(key, params);
+    this.store.set(storeKey, value);
   }
   has(key: string, params: unknown) {
-    const paramsKey = JSON.stringify(params);
-    return this.store[key]?.has(paramsKey) ?? false;
+    const storeKey = this.getStoreKey(key, params);
+
+    return this.store.has(storeKey);
   }
   clear(key: string, params: unknown) {
-    const paramsKey = JSON.stringify(params);
-    const query = this.store[key];
-    if (!query) return;
-    query.delete(paramsKey);
+    const storeKey = this.getStoreKey(key, params);
+    this.store.delete(storeKey);
   }
 }
